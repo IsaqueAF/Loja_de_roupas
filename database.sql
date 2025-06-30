85064a2b2b130d637f2235ea27df48da67fc35d7
IsaqueAF <isaqueaf9307286@gmail.com>
2025-06-30 14:16:52 -0300

    Início
    Adicionando código já feito

---
+++ database.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE loja;
+USE loja;
+
+-- produto
+CREATE TABLE roupa (
+	id INT AUTO_INCREMENT PRIMARY KEY,
+    nome VARCHAR(100) NOT NULL,
+    peso FLOAT NOT NULL,
+    quantidade INT NOT NULL,
+    descricao TEXT NOT NULL,
+    cor VARCHAR(20) NOT NULL,
+    tamanho VARCHAR(20) NOT NULL
+);
+
+-- pessoa
+CREATE TABLE IF NOT EXISTS nivel (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    valor VARCHAR(20)
+);
+
+CREATE TABLE IF NOT EXISTS contatos (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    telefone CHAR(14),
+    telefone_de_emergencia CHAR(14) NOT NULL,
+    email VARCHAR(70) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS endereco (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    numero INT,
+    rua VARCHAR(100),
+    bairro VARCHAR(100),
+    cidade VARCHAR(100),
+    estado VARCHAR(100),
+    CEP CHAR(9)
+);
+
+CREATE TABLE IF NOT EXISTS pessoa (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    contatos INT NOT NULL,
+    endereco INT NOT NULL,
+    nivel INT NOT NULL,
+    nome VARCHAR(50) NOT NULL,
+    data_de_nascimento VARCHAR(10) NOT NULL,
+    CPF INT NOT NULL,
+    FOREIGN KEY (nivel) REFERENCES nivel(id),
+    FOREIGN KEY (contatos) REFERENCES contatos(id),
+    FOREIGN KEY (endereco) REFERENCES endereco(id)
+);
+
+-- compra
+CREATE TABLE compra (
+	token INT AUTO_INCREMENT PRIMARY KEY,
+    id_comprador INT,
+    id_vendedor INT,
+    hora VARCHAR(8),
+    endereco_de_entrega INT,
+    FOREIGN KEY (id_comprador) REFERENCES pessoa(id),
+    FOREIGN KEY (id_vendedor) REFERENCES pessoa(id),
+    FOREIGN KEY (endereco_de_entrega) REFERENCES endereco(id)
+);
---
+++ main.py
@@ -0,0 +1,48 @@
+import mysql.connector as connector
+
+def connect ():
+    return connector.connect(
+        host="127.0.0.1",
+        port=3306,
+        user="root",
+        password="aluno",
+        database="loja"
+    )
+
+def CadastrarProduto (cursor):
+    
+    nome = input("Digite o nome da sua roupa: ")
+    quantidade = int(input("Digite a quantidade da sua roupa: "))
+    peso = float(input("Digite o peso da sua roupa: "))
+    descricao = input("Digite a descrição da sua roupa: ")
+    cor = input("Digite a cor da sua roupa: ")
+    tamanho = input("Digite o tamanho da sua roupa: ")
+
+    cursor.execute("""
+        INSERT INTO roupa(nome,quantidade,peso,descricao,cor,tamanho)
+        VALUES (%s,20,12,%s,%s,%s);
+    """, (nome,descricao,cor,tamanho))
+
+    print("Cadastro feito com sucesso")
+    
+
+while True:
+
+    print("\n   ---{ + LOJA DE ROUPA + }---\n")
+    
+    print("1 - cadastrar produto")
+
+    responce = int(input("\n\nEscolha -> "))
+
+    try:
+
+        connection = connect()
+        cursor = connection.cursor()
+
+        if responce == 1:
+            CadastrarProduto(cursor)
+        else:
+            print("valor inválido")
+
+    except connector.Error as error:
+        print(error)
